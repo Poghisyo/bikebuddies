f44c116c1bbea5805bece559cc8448a7073f226c
Ryan Wiens <ryaninshanghai@gmail.com>
2017-11-15 15:49:00 +0100

    made landing page layout

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,6 +1,7 @@
 class BikesController < ApplicationController
    skip_before_action :authenticate_user!, only: [:index, :show]
 
+
   def index
     @bikes = Bike.all
   end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @bikes = Bike.all
+    @three_bikes = @bikes.take(3)
   end
 end
