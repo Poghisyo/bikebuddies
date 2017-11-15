57b9b34ebe001ae975768ad561d1440967509314
victor laurent <victor.laurent@gmail.com>
2017-11-15 16:40:10 +0100

    updated stylsheets and changed controller for search bar and changed navbar view

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -2,8 +2,19 @@ class BikesController < ApplicationController
    skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @bikes = Bike.all
+    @filters = params.permit(:location)
+
+    if @filters.present?
+      @bikes = Bike.where(@filters)
+    else
+      @bikes = Bike.all
+    end
   end
+
+  # def top
+  #   Nice to implement a top price or top quality filter
+  #   @bikes = Bike.where(price: XX)
+  # end
 
   def show
     @bike = Bike.find(params[:id])
