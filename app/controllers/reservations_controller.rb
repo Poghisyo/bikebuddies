280e28bda0499d29b88e2717d15e2e5e5cc511fa
Ryan Wiens <ryaninshanghai@gmail.com>
2017-11-17 12:24:43 +0100

    make a reservations list

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,10 @@
 class ReservationsController < ApplicationController
   def index
+    # @reservations = current_user.reservations
+    @pending_reservations = current_user.reservations.where(status: "pending")
   end
 
-  def show
-  end
+  # def show
+  #   @reservation = Rental.find(params[:id])
+  # end
 end
