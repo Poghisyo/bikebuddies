b265f36b4f41c2995d28c369b72ae248c05a912b
Ryan Wiens <ryaninshanghai@gmail.com>
2017-11-17 14:37:26 +0100

    add reservation index page

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,10 +1,13 @@
 class ReservationsController < ApplicationController
   def index
-    # @reservations = current_user.reservations
-    @pending_reservations = current_user.reservations.where(status: "pending")
+    @bikes = Bike.where(seller: current_user)
+
+    # @pending_reservations = current_user.reservations.where(status: "pending")
+    # @reservations = current_user.rentals.group_by(&:status)
   end
 
-  # def show
-  #   @reservation = Rental.find(params[:id])
-  # end
+  def show
+    @reservation = Rental.find(params[:id])
+    @bike = @reservation.bike
+  end
 end
